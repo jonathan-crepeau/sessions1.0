7afc9f0f69e258ae6dc4bb344bac2755d0ea4a3b
jonathan-crepeau <jon.crepeau@gmail.com>
2022-11-29 17:45:10 -0800

    initial db connection successful

--- server.js
+++ server.js
@@ -1,6 +1,18 @@
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3454;
+const mongoose = require('mongoose');
+const mongoURI = "mongodb+srv://jonathan-troy:black-pink@sessionsdeargodcluster0.iiymajh.mongodb.net/sessions-dear-god?retryWrites=true&w=majority";
+
+mongoose.connect(mongoURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+    .then(() => console.log('Initial DB connection successful..'))
+    .catch((error) => console.log(error));
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
 
 app.get('/', (req, res) => {
     res.send('<h1>Homepage</h1>');
